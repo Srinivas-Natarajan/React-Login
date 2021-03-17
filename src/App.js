d6d3b9cd6cb16215a59b83e9dce9c8ae194c26e2
Srinivas Natarajan <nsrinivas06@gmail.com>
2021-03-18 01:17:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import LoginForm2 from './components/LoginForm2';
+
 import './App.css';
 
 function App() {
+  const admin = {
+    email:"admin@gmail.com",
+    password:"abcd"
+  }
+
+  const [user,setUser] = useState({name:"", email:""});
+  const [error,setError] = useState("")
+
+  const Login = details =>{
+    if(details.email === admin.email && details.password === admin.password){
+      console.log("Logged in!!");
+      setUser({
+        email:details.email,
+      })
+    }
+    else{
+      console.log("Dont match");
+      setError("Incorrect Details");
+    }
+  }
+
+  const Logout = () =>{
+    console.log("Logout");
+    setUser({
+      email:"",
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      { (user.email !== "")?(
+        <div className="welcome">
+          <h2>Welcome <span>{user.email}</span></h2>
+          <button onClick={Logout}>Logout</button>
+        </div>
+      ) : (
+          <div>
+            <div className="split left">
+              <div class="centered">
+                <h2>School Name</h2>
+              </div>
+            </div>
+            <div className="split right">
+              <div class="centered">
+                <LoginForm2 Login={Login} error={error} />
+              </div>
+            </div>
+          </div> 
+      ) }
     </div>
   );
 }
